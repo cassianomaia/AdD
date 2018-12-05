f33ca1e5efe667e1652368f940995f028707b2b6
Cassiano Maia <cassmala@gmail.com>
2018-12-04 22:22:00 -0200

    Criação do glide, que será utilizado para baixar as imagens através dos links que recebermos da api.

---
+++ app/src/main/java/catito/add/Utils/MyAppGlideModule.kt
@@ -0,0 +1,8 @@
+package catito.add.Utils
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+
+@GlideModule
+public class MyAppGlideModule: AppGlideModule()
