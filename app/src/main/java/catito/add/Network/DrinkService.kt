c32574d8e6b4167a160fef2b3f35efc9f18bf27a
Cassiano Maia <cassmala@gmail.com>
2018-12-05 02:14:44 -0200

    Adcionando query para pegar um drrink pelo ID.

--- app/src/main/java/catito/add/Network/DrinkService.kt
+++ app/src/main/java/catito/add/Network/DrinkService.kt
@@ -1,12 +1,18 @@
 package catito.add.Network
 
+import catito.add.entities.Drink
 import catito.add.entities.DrinkList
 import retrofit2.http.GET
 import retrofit2.Call
+import retrofit2.http.Query
 
 interface DrinkService {
 
     //Função para obter todos os drinks alcoolicos
     @GET("filter.php?a=Alcoholic")
     fun getLista(): Call<DrinkList>
+
+    //Função para obter um drink através do ID
+    @GET("lookup.php?i=")
+    fun getDrinkID(@Query("i")query: String): Call<DrinkList>
 }
