c65cdac5939f0a23670fb5e2d2cec41ba6876817
Cassiano Maia <cassmala@gmail.com>
2018-12-05 02:19:47 -0200

    Adcionando clickListener a recycler view.

--- app/src/main/java/catito/add/scenario_main/DrinkAdapter.kt
+++ app/src/main/java/catito/add/scenario_main/DrinkAdapter.kt
@@ -12,8 +12,14 @@ import kotlinx.android.synthetic.main.drink_item.view.*
 
 class DrinkAdapter (val context: Context, val drinks: List<Drink>): RecyclerView.Adapter<DrinkAdapter.ViewHolder>() {
 
+    var ItemClickListener: ((index: Int) -> Unit)? = null
+
+    fun setOnItemClickListener(clique: ((index: Int)-> Unit)){
+        this.ItemClickListener = clique
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindView(context, drinks[position])
+        holder.bindView(context, drinks[position], ItemClickListener)
     }
 
     override fun getItemCount(): Int {
@@ -25,8 +31,9 @@ class DrinkAdapter (val context: Context, val drinks: List<Drink>): RecyclerView
         return ViewHolder(view)
     }
 
+
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        fun bindView(context: Context, drink: Drink) {
+        fun bindView(context: Context, drink: Drink, itemClickListener: ((index: Int) -> Unit)?) {
 
             GlideApp.with(context)
                 .load(drink.strDrinkThumb)
@@ -34,6 +41,12 @@ class DrinkAdapter (val context: Context, val drinks: List<Drink>): RecyclerView
                 .into(itemView.imgDrink)
 
             itemView.txtDrink.text = drink.strDrink
+
+            if(itemClickListener != null){
+                itemView.setOnClickListener{
+                    itemClickListener.invoke(adapterPosition)
+                }
+            }
         }
     }
 }
